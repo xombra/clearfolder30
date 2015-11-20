3c2e090be8b142a52c4669a7cd1eb3cf98bfea27
Hector A. Mantellini <xombra@users.noreply.github.com>
2015-11-19 23:06:32 -0430

    Script que elimina el contenido (archivos)
    Los archivos que tengan mas de 30 min en una carpeta son eliminados

---
+++ delfolder.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# by Xombra
+# Licence: GPL-2
+# Clear contents of a folder every 30 min
+
+fechaactualmenos30min=$(date +%s --date='-30 min')
+fechaactual=$(date)
+SAVEIF=$IFS
+
+directorio="temporales"
+
+IFS=$(echo -en "\n\b")
+
+if [ -d $directorio ]; then
+
+	for file in $(ls $directorio)
+		do
+
+			archivo=${file%%}
+			fechaarchivo=$(stat -c "%Y" $archivo)
+			fechafile=${fechaarchivo%% *}
+			echo "* $fechaarchivo | $fechaactualmenos30min"
+			if [ $fechaarchivo -lt $fechaactualmenos30min ]; then
+				rm -f -R  temporales/$archivo
+				echo "Eliminado fecha - $fechaactual | Archivo $archivo" >> /var/log/tempBorrados.log
+			fi
+
+		done
+
+else
+	echo "No existe el Directorio -> $directorio "
+fi
+
+IFS=$SAVEIFS
+exit 0
